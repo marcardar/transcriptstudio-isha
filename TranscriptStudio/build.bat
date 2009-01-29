590b6f97ed12b5e01ec113c520c41138cc02009d
mjc1904 <mjc1904@b4a5fc70-cd90-11dd-a8b6-f5aca7b582bb>
2009-01-29 12:51:22 +0000

    Now ANT script compiles the swf - user should probably set the FLEX_HOME system variable.

--- TranscriptStudio/build.bat
+++ TranscriptStudio/build.bat
@@ -9,16 +9,21 @@ if not "%JAVA_HOME%" == "" goto gotJavaHome
 set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_07
 
 :gotJavaHome
+if not "%FLEX_HOME%" == "" goto gotFlexHome
+set FLEX_HOME=C:\Program Files\Adobe\Flex Builder 3\sdks\3.2.0
+
+:gotFlexHome
 set ANT_HOME=%EXIST_HOME%\tools\ant
 set _LIBJARS=%CLASSPATH%;%ANT_HOME%\lib\ant-launcher.jar;%ANT_HOME%\lib\junit-4.4.jar;%JAVA_HOME%\lib\tools.jar
 
 set JAVA_ENDORSED_DIRS=%EXIST_HOME%\lib\endorsed
-set JAVA_OPTS=-Xms64M -Xmx512M -Djava.endorsed.dirs="%JAVA_ENDORSED_DIRS%" -Dant.home="%ANT_HOME%" -Dexist.home="%EXIST_HOME%"
+set JAVA_OPTS=-Xms64M -Xmx512M -Djava.endorsed.dirs="%JAVA_ENDORSED_DIRS%" -DFLEX_HOME="%FLEX_HOME%" -Dant.home="%ANT_HOME%" -Dexist.home="%EXIST_HOME%"
 
 echo Isha Foundation Transcript Studio eXist-Modules Build
 echo -------------------
 echo JAVA_HOME=%JAVA_HOME%
 echo EXIST_HOME=%EXIST_HOME%
+echo FLEX_HOME=%FLEX_HOME%
 echo _LIBJARS=%_LIBJARS%
 
 echo Starting Ant...
