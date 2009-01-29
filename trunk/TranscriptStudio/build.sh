e1c27db1fa8816dba71b7cdce20bfe377c1e59bb
mjc1904 <mjc1904@b4a5fc70-cd90-11dd-a8b6-f5aca7b582bb>
2009-01-29 12:51:22 +0000

    Now ANT script compiles the swf - user should probably set the FLEX_HOME system variable.

--- trunk/TranscriptStudio/build.sh
+++ trunk/TranscriptStudio/build.sh
@@ -13,6 +13,10 @@ if [ ! -d "$JAVA_HOME" ]; then
     JAVA_HOME="/usr/lib/jvm/java-6-sun-1.6.0.07/jre"
 fi
 
+if [ -z "$FLEX_HOME" ]; then
+    JAVA_HOME="/usr/lib/Adobe_Flex_Builder/sdks/3.2.0"
+fi
+
 JAVA_CMD="$JAVA_HOME/bin/java"
 
 ANT_HOME="$EXIST_HOME/tools/ant"
@@ -21,7 +25,7 @@ LOCALCLASSPATH=$CLASSPATH:$ANT_HOME/lib/ant-launcher.jar:$ANT_HOME/lib/junit-4.4
 
 JAVA_ENDORSED_DIRS="$EXIST_HOME"/lib/endorsed
 
-JAVA_OPTS="-Dant.home=$ANT_HOME -Djava.endorsed.dirs=$JAVA_ENDORSED_DIRS -Dexist.home=$EXIST_HOME"
+JAVA_OPTS="-DFLEX_HOME=$FLEX_HOME -Dant.home=$ANT_HOME -Djava.endorsed.dirs=$JAVA_ENDORSED_DIRS -Dexist.home=$EXIST_HOME"
 
 echo Starting Ant...
 echo
